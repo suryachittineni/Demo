4d8c9a22a24c6b39acfd1a1dc58c051e22fb16a8
surya <surya@surya-PC>
2013-05-11 19:40:35 -0500

    Added another print statement

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -15,5 +15,7 @@ public class Demo {
      */
     public static void main(String[] args) {
         System.out.println("My name is XYZ");
+        System.out.println("My name is Surya");
+
        }
 }
