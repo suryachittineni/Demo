0cf6fbcf30d1bf2a8ee9c55b2586489bd906ac9b
surya <surya@surya-PC>
2013-05-11 19:35:35 -0500

    Added a print statement

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -14,6 +14,6 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.println("My name is XYZ");
+       }
 }
